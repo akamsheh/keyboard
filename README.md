7389d88074bf6851eba82644a27a972ef3e2de18
adam kamsheh <>
2022-05-04 22:40:51 -0400

    planck ez

---
+++ README.md
@@ -0,0 +1,9 @@
+# Keyboard Fun
+
+## Planck EZ
+
+### [ReadMe](#planck-ez)
+
+### Current Layout:
+
+![!](docs/layout.webp)
---
+++ planck_ez/src/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define NO_AUTO_SHIFT_SPECIAL
+#define FIRMWARE_VERSION u8"KZ7rX/gDOL4"
+#define RGB_MATRIX_STARTUP_SPD 60
